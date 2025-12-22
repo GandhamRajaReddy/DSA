22e6cef18a0b7fe579efc3bef16e80abafd92c22
GandhamRajaReddy <gandhamrajareddy2@gmail.com>
2025-12-22 19:21:39 +0530

    1q

---
+++ 2pointers__slidingwindow/longestsubstring.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    /* Function to find the longest substring
+    without repeating characters*/
+    int longestNonRepeatingSubstring(string& s) {
+       int n=s.size();
+       int hash[256];
+       for(int i=0;i<256;i++)
+       {
+        hash[i]=-1;
+       }
+       int l=0,r=0,maxlen=0;
+       while(r<n)
+       {
+        if(hash[s[r]]!=-1)
+        {
+            if(hash[s[r]]>=l)
+            {
+                l=hash[s[r]]+1;
+            }
+        }
+        int len=r-l+1;
+        maxlen=max(len,maxlen);
+        hash[s[r]]=r;
+        r++;
+
+       }
+       return maxlen;
+    }
+};
+
+int main() {
+    string s = "cadbzabcd"; 
+    
+    // Create an instance of the Solution class
+    Solution sol;
+
+    int result = sol.longestNonRepeatingSubstring(s);
+
+
+    cout << "The maximum length is:\n";
+    cout << result << endl;
+
+    return 0;
+}
